08b0f45ff5da3d687e3b1e69c0ec8f0f3ec042fe
Matheus <szmatheussouza@gmail.com>
2024-08-07 20:38:21 -0300

    rotas post e put

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
 
+app.post("/cadastro", function (req, res) {
+    res.json({
+        status: "OK",
+        message: "Cadastrado com sucesso"
+    })
+});
+
 app.get("/", function (req, res) {
     res.send("Hello World")
 });
+
+app.put("/cadastro/1", function (req, res) {
+    res.json({
+        status: "OK",
+        message: "Editado com sucesso."
+    })
+})
 
 app.listen(3000);
